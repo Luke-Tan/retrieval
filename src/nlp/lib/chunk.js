9a3a2684a870523e07dee811b5a949d9f29f301a
Luke Tan <luketan1998@gmail.com>
2023-06-14 18:07:56 +0800

    test: do not remove numbers when chunking

--- src/nlp/lib/chunk.js
+++ src/nlp/lib/chunk.js
@@ -1,28 +1,27 @@
 /*
- * Functions that chunk text in 2 steps: strip non-letters first, 
+ * Functions that chunk text in 2 steps: strip non-letters first,
  * then chunk into unigrams.
  */
 
-module.exports = (function(){
-	/**
-	 * 1. Clean up sentences: all non-letters replaced with a whitespace.
-	 */
-	function keepOnlyLetters(sentence){
-	  return sentence.replace(/[^a-z]/gi, ' ');
-	};
+module.exports = (function () {
+  /**
+   * 1. Clean up sentences: all non-letters replaced with a whitespace.
+   */
+  function keepOnlyLetters(sentence) {
+    return sentence.replace(/[^a-z1-9]/gi, " ");
+  }
 
-	/**
-	 * 2. Chunk text using whitespace as the separator
-	 */
-	function getChunks(sentence){
-	  var camelCaseSplit = sentence.replace(/([a-z](?=[A-Z]))/g, '$1 ');
-	  return camelCaseSplit.match(/\S+/g) || [];
-	};
+  /**
+   * 2. Chunk text using whitespace as the separator
+   */
+  function getChunks(sentence) {
+    var camelCaseSplit = sentence.replace(/([a-z](?=[A-Z]))/g, "$1 ");
+    return camelCaseSplit.match(/\S+/g) || [];
+  }
 
-	return function(sentence) {
-            var onlyLetters = keepOnlyLetters(sentence);
-            var tokens = getChunks(onlyLetters);
-            return tokens;
-           };
-
+  return function (sentence) {
+    var onlyLetters = keepOnlyLetters(sentence);
+    var tokens = getChunks(onlyLetters);
+    return tokens;
+  };
 })();
